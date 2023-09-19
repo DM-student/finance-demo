29ed1454482192884a80869fe30ab344128e3f0b
DM <dima.markov2003@gmail.com>
2023-09-19 14:33:04 +0300

    initial commit

---
+++ src/main/kotlin/demo/financeproject/FinanceProjectApplication.kt
@@ -0,0 +1,11 @@
+package demo.financeproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceProjectApplication>(*args)
+}
---
+++ src/test/kotlin/demo/financeproject/FinanceProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package demo.financeproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
